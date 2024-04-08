ae815e2094774fab188d088a1517573a73fd4829
youbrajrai <youbrajrai79@gmail.com>
2024-04-08 22:51:05 +0545

    first commit

---
+++ fcd_community/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fcd_community.users.models import User
+from fcd_community.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
