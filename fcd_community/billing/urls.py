7f9c3e1db0180cb17e707d26c1c14135673d365d
youbrajrai <youbrajrai79@gmail.com>
2024-04-14 14:06:08 +0545

    new app billing added

---
+++ fcd_community/billing/urls.py
@@ -0,0 +1,7 @@
+    # from django.urls import path
+    # from .views import billing_home
+
+    # app_name = "billing"
+    # urlpatterns = [
+    #     path("", view=billing_home, name="home"),
+    # ]
---
+++ fcd_community/billing/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render
+# from django.http import HttpRequest
+
+
+# # Create your views here.
+# def billing_home(request: HttpRequest):
+#     return render(request, "billing\billing_home.html")
