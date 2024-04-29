145ba3ab01152021dbcfe3550c6daeb4689c842f
youbrajrai <youbrajrai79@gmail.com>
2024-04-29 11:46:10 +0545

    change public ip to domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,7 +17,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["122.184.78.196"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["youbrajrai.com.np"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -112,7 +112,7 @@ STATIC_URL = f"https://{aws_s3_domain}/static/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Full Cycle Dev Community <noreply@122.184.78.196>",
+    default="Full Cycle Dev Community <noreply@youbrajrai.com.np>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
--- fcd_community/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ fcd_community/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -40,7 +40,7 @@ def update_site_forward(apps, schema_editor):
     _update_or_create_site_with_sequence(
         Site,
         schema_editor.connection,
-        "122.184.78.196",
+        "youbrajrai.com.np",
         "Full Cycle Dev Community",
     )
 
